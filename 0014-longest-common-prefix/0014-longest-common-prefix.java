27dfa58faa7beffad0177ad4f8ffc8558240ad14
Abhijit Balasaheb Godase <abhijitgodase04.10@gmail.com>
2026-07-26 20:55:30 +0530

    Time: 4 ms (12.39%), Space: 43.6 MB (7.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,16 +1,32 @@
-class Solution {
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//  int len = strs.length;
+//         String Pref = "";
+//        for(int i = 0 ; i < strs[0].length() ; i++){
+//          char ch = strs[0].charAt(i) ;
+//             for(String str : strs){
+//                 if(i >= str.length() || ch != str.charAt(i)  ){
+//                     return Pref;
+//                 }
+//             }
+//             Pref += ch;
+//        }
+//         return Pref;
+//     }
+// }
+
+
+class Solution{
     public String longestCommonPrefix(String[] strs) {
- int len = strs.length;
-        String Pref = "";
-       for(int i = 0 ; i < strs[0].length() ; i++){
-         char ch = strs[0].charAt(i) ;
-            for(String str : strs){
-                if(i >= str.length() || ch != str.charAt(i)  ){
-                    return Pref;
-                }
+        Arrays.sort(strs);
+        String pref = "";
+        int min = Math.min( strs[0].length(),strs[strs.length -1].length());
+        for(int i = 0 ; i < min ; i++){
+            if( strs[0].charAt(i) != strs[strs.length -1].charAt(i)){
+                return pref;
             }
-            Pref += ch;
-       }
-        return Pref;
+            pref += strs[0].charAt(i);
+        }
+        return pref;
     }
 }
