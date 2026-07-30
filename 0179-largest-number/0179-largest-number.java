d1c0098820d4fa60146a8a9f45c551de93d74972
Abhijit Balasaheb Godase <abhijitgodase04.10@gmail.com>
2026-07-30 21:19:11 +0530

    Time: 6 ms (94.46%), Space: 45.3 MB (12.99%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,66 +1,4 @@
-// class Solution {
-
-//     public String[] convert(int[] nums) {
-//         String[] result = new String[nums.length];
-
-//         for (int i = 0; i < nums.length; i++) {
-//             result[i] = String.valueOf(nums[i]);
-//         }
-
-//         return result;
-//     }
-
-//     public String largestNumber(int[] nums) {
-
-//         String[] arr = convert(nums);
-
-//         for (int i = 0; i < arr.length - 1; i++) {
-//             for (int j = 0; j < arr.length - 1 - i; j++) {
-//                 int a = 0;
-//                 int lenA = arr[j].length();
-//                 int lenB = arr[j + 1].length();
-//                 int min = Math.min(lenA, lenB);
-
-//                 while (a <= min) {
-
-//                     if(a==min){
-//                     if (((lenA > lenB) && (arr[j].charAt(a) < arr[j].charAt(0))) ||
-//                             ((lenA < lenB) && (arr[j + 1].charAt(a) > arr[j + 1].charAt(0)))) {
-
-//                         String temp = arr[j];
-//                         arr[j] = arr[j + 1];
-//                         arr[j + 1] = temp;
-
-//                         break;
-//                     }
-//                     }else{
-//                     if (arr[j].charAt(a) == arr[j + 1].charAt(a)) {
-//                         a++;
-//                         continue;
-//                     }
-
-//                     if (arr[j].charAt(a) < arr[j + 1].charAt(a)) {
-//                         String temp = arr[j];
-//                         arr[j] = arr[j + 1];
-//                         arr[j + 1] = temp;
-//                         break;
-//                     }
-//                     }
-//                 }
-//             }
-//         }
-
-//         String res = "";
-//         int st = 0;
-//         while (st < arr.length) {
-//             res += arr[st];
-//             st++;
-//         }
-
-//         return res;
-//     }
-// }
-
+ 
 class Solution {
 
     public String[] convert(int[] nums) {
@@ -76,24 +14,9 @@ class Solution {
     public String largestNumber(int[] nums) {
 
         String[] arr = convert(nums);
-
-
-        
-
-
-        for (int i = 0; i < arr.length ; i++) {
-
-            for (int j = 0; j < arr.length -1 - i; j++) {
-                if ((arr[j] + arr[j + 1])
-                        .compareTo(arr[j + 1] + arr[j]) < 0) {
-
-                    String temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    
-                }
-            }
-        }
+  Arrays.sort(arr, (a, b) -> {
+            return (b + a).compareTo(a + b);
+        });
   String res = "";
     int st = 0;
     while(st<arr.length)
