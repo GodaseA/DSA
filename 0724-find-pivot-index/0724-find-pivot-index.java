ffde342bf871b14e69c6730c036e49a2d90d443b
Abhijit Balasaheb Godase <abhijitgodase04.10@gmail.com>
2026-09-06 12:17:21 +0530

    Time: 1 ms (97.05%), Space: 47.2 MB (88.6%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -12,9 +12,7 @@ class Solution {
 
 
         for(int i = 0 ; i <nums.length-1 ; i++){
-            if(i == nums.length-1 && nums[0]  == nums[i]){
-                return i;
-            }
+
             if(nums[0] - nums[i+1] == nums[i]){
                 return i;
             }
