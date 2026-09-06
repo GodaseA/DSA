b9e78f520af1715be4f5635b54284ffdfff6030f
Abhijit Balasaheb Godase <abhijitgodase04.10@gmail.com>
2026-09-06 23:39:07 +0530

    Time: 26 ms (33.84%), Space: 48.7 MB (60.02%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,25 +1,58 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+
+//         for(int i = 0 ; i < nums.length ; i++){
+//             if(i == 0){
+//                 nums[i] = nums[i];
+//             }else{
+//                 nums[i] = nums[i] + nums[i-1];
+//             }
+//         }
+//          int count = 0;
+//          for(int j = 0 ; j < nums.length ; j++){
+//              if(nums[j] == k){
+//                     count++;
+//                 }
+//             for(int l = j + 1 ; l < nums.length ; l++){
+//                 if(nums[l] - nums[j]  == k){
+//                     count++;
+//                 }
+//             }
+//          }
+//          return count;
+//     }
+// }
+
+
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
+
+        HashMap <Integer , Integer > map = new HashMap<>();
+        map.put(0,1);
+        int count = 0;
 
         for(int i = 0 ; i < nums.length ; i++){
             if(i == 0){
                 nums[i] = nums[i];
+                count = count + map.getOrDefault(nums[i] - k , 0);
+                map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
             }else{
                 nums[i] = nums[i] + nums[i-1];
+                count = count + map.getOrDefault(nums[i] - k , 0);
+                map.put(nums[i],map.getOrDefault(nums[i],0) + 1);
             }
+
         }
 
-         int count = 0;
-         for(int j = 0 ; j < nums.length ; j++){
-             if(nums[j] == k){
-                    count++;
-                }
-            for(int l = j + 1 ; l < nums.length ; l++){
-                if(nums[l] - nums[j]  == k){
-                    count++;
-                }
-            }
-         }
+        //   if(k == 0 && nums[0] != 0){
+        //         count = -1;
+        //     }
+        //  for(int j = 0 ; j < nums.length ; j++){
+           
+        //     count = count + map.getOrDefault(nums[j] - k , 0);
+ 
+        //  }
          return count;
     }
 }
