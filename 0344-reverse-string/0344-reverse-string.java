749fe74fb118539609c6e506fb785a98fad58ee2
Abhijit Balasaheb Godase <abhijitgodase04.10@gmail.com>
2026-09-13 11:59:48 +0530

    Time: 1 ms (21.36%), Space: 48.2 MB (73.85%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int a = 0; 
+        int b = s.length -1 ;
+
+        while(a<b){
+            char t = s[a];
+            s[a] = s[b];
+            s[b] = t;
+
+            a++;
+            b--;
+        }
+        
+    }
+}
